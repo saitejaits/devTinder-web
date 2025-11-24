24cfea2e39c7398c85316765608266efe47aa951
bommalisaiteja <saiteja.bommali@orgchats.com>
2025-11-24 22:25:42 +0530

    redirect to login if not authenticated

--- README.md
+++ README.md
@@ -21,7 +21,9 @@
 - Login and see if your data is coming properly in the store
 - NavBar should update as soon as user logs in
 - Refactor our code to add constants file + create a component folder
-
+- You should not be access other routes without login
+- If token is not present, redirect user to login page
+-
 
 - Body 
 -       NavBar
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import NavBar from "./NavBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data));
+    } catch (error) {
+      if (error.status) {
+        navigate("/login");
+      }
+      console.log("fetchUser ERROR:-", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!userData) {
+      fetchUser();
+    }
+  }, []);
+
   return (
     <div>
-        <NavBar />
-        <Outlet />
-        <Footer />
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
